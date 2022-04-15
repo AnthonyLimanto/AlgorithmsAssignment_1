c94461988fa632401c75c68481469787d9d66801
Anthony Limanto <s3898935@student.rmit.edu.au>
2022-04-15 16:44:29 +1000

    I tried to implementthe tst  and search and insert

--- Assign1-s3898395-s3781318/dictionary/ternarysearchtree_dictionary.py
+++ Assign1-s3898395-s3781318/dictionary/ternarysearchtree_dictionary.py
@@ -1,3 +1,5 @@
+
+from pkg_resources import working_set
 from dictionary.base_dictionary import BaseDictionary
 from dictionary.word_frequency import WordFrequency
 from dictionary.node import Node
@@ -18,7 +20,12 @@ class TernarySearchTreeDictionary(BaseDictionary):
         construct the data structure to store nodes
         @param words_frequencies: list of (word, frequency) to be stored
         """
+        self.root = None
         # TO BE IMPLEMENTED
+        for words in words_frequencies:
+            self.add_word_frequency(words)
+
+
 
     def search(self, word: str) -> int:
         """
@@ -27,8 +34,13 @@ class TernarySearchTreeDictionary(BaseDictionary):
         @return: frequency > 0 if found and 0 if NOT found
         """
         # TO BE IMPLEMENTED
+        node = self._search(self.root, word, 0)
+        if node == None:
+            return 0
+        
+
         # place holder for return
-        return 0
+        return node.frequency
 
     def add_word_frequency(self, word_frequency: WordFrequency) -> bool:
         """
@@ -37,8 +49,15 @@ class TernarySearchTreeDictionary(BaseDictionary):
         :return: True whether succeeded, False when word is already in the dictionary
         """
         # TO BE IMPLEMENTED
+            
+        if self.search(word_frequency.word) == 0:
+            return False
+            
+        self.root = self.insert(self.root, word_frequency.word, word_frequency.frequency, 0)
+
+        # use search to if is in dict already
         # place holder for return
-        return False
+        return True
 
     def delete_word(self, word: str) -> bool:
         """
@@ -59,3 +78,45 @@ class TernarySearchTreeDictionary(BaseDictionary):
         # TO BE IMPLEMENTED
         # place holder for return
         return []
+
+
+    # I put this here
+    def insert(self, root, string, frequency, index):
+
+        char = string[index]
+
+        if root is None:
+            root = Node(letter= char)
+
+        if char < root.letter:
+            root.left = self.insert(root.left, string, frequency, index)
+        
+        elif char > root.letter:
+            root.right = self.insert(root.right, string, frequency, index)
+
+        elif index < len(string) - 1:
+            root.middle = self.insert(root.middle, string, frequency, index + 1)
+        else:
+            root.frequency = frequency
+            root.end_word = True
+
+        return root
+    
+    # i put this here
+    def _search(self, node, string, index):
+
+        if node == None:
+            return None
+
+        char = string[index]
+
+        if char < node.letter:
+            node.left = self._search(node.left, string, index)
+        
+        elif char > node.letter:
+            node.right = self._search(node.right, string, index)
+
+        elif index < len(string) - 1:
+            node.middle = self._search(node.middle, string, index + 1)
+        else:
+            return node
